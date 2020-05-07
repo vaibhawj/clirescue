319851c7eda89e9d037de2a55ad6b22a3584328f
vaibhawj <vj.vaibhaw@gmail.com>
2020-05-07 20:05:36 +0530

    forking repo requires internal code changes

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	"github.com/GoBootcamp/clirescue/trackerapi"
+	"github.com/vaibhawj/clirescue/trackerapi"
 	"github.com/codegangsta/cli"
 )
 
--- trackerapi/me.go
+++ trackerapi/me.go
@@ -8,8 +8,8 @@ import (
 	"os"
 	u "os/user"
 
-	"github.com/GoBootcamp/clirescue/cmdutil"
-	"github.com/GoBootcamp/clirescue/user"
+	"github.com/vaibhawj/clirescue/cmdutil"
+	"github.com/vaibhawj/clirescue/user"
 )
 
 var (
